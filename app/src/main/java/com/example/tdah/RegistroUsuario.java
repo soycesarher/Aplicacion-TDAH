2ff5695623e64e1ded18b6f0c2b51e38e957d954
scr47One <hgonzalezg@toluca.tecnm.mx>
2021-11-01 13:38:29 -0600

    se agregó padding a las cajas de texto y botones de la interfaz

--- app/src/main/java/com/example/tdah/RegistroUsuario.java
+++ app/src/main/java/com/example/tdah/RegistroUsuario.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
