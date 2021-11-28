52b73550b3fa7d4df49783a06d7a5f573bbc64b7
scr47One <hgonzalezg@toluca.tecnm.mx>
2021-11-28 16:40:46 -0600

    intefaz audio y acciones de boton

--- app/src/main/java/com/example/tdah/audio/Audio.java
+++ app/src/main/java/com/example/tdah/audio/Audio.java
@@ -1,18 +1,13 @@
 package com.example.tdah.audio;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.example.tdah.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.SuccessContinuation;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.ListResult;
+
 import com.google.firebase.storage.StorageReference;
 
 import java.io.IOException;
@@ -48,24 +43,22 @@ public class Audio extends AppCompatActivity {
     public void listAllFiles() {
         FirebaseStorage storage = FirebaseStorage.getInstance();
         // [START storage_list_all]
-        StorageReference listRef = storage.getReference().child("files/uid");
+        StorageReference listRef = storage.getReference().child("files");
 
         listRef.listAll()
                 .addOnSuccessListener(listResult -> {
                     for (StorageReference prefix : listResult.getPrefixes()) {
                         // All the prefixes under listRef.
                         // You may call listAll() recursively on them.
-
                     }
                     for (StorageReference item : listResult.getItems()) {
                         // All the items under listRef.
-
+                        item.getDownloadUrl();
                     }
                 })
                 .addOnFailureListener(e -> {
                     // Uh-oh, an error occurred!
                 });
-        // [END storage_list_all]
     }
 
     public  void pausa(String url) {
